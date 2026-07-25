9bfd839955e320a7612d4483d0cecd30088d5f00
RIYA MISHRA <itsriya1586@gmail.com>
2026-07-25 22:38:55 +0530

    04_Basic_Queries.sql

--- 04_Basic_Queries.sql
+++ 04_Basic_Queries.sql
@@ -8,3 +8,30 @@ SELECT employee_name,city FROM Employees;
 SELECT * FROM Employees WHERE city='Noida'; 
 
 SELECT * FROM Employees WHERE age>25;
+
+-- Update
+UPDATE Employees
+SET city='Gurgaon'
+WHERE employee_id=3;
+
+-- Delete
+DELETE FROM Employees
+WHERE employee_id=10;
+
+-- Total Salary
+SELECT
+employee_id,
+basic_salary+bonus-deduction AS Total_Salary
+FROM Salary;
+
+-- Average Salary
+SELECT AVG(basic_salary)
+FROM Salary;
+
+-- Total Salary Expense
+SELECT SUM(basic_salary)
+FROM Salary;
+
+-- Employee Count
+SELECT COUNT(*)
+FROM Employees;
