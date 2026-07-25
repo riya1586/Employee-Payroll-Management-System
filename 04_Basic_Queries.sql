88ba1b41adfbf12b62d2049fcb1085aabcfe8129
RIYA MISHRA <itsriya1586@gmail.com>
2026-07-25 22:32:22 +0530

    Create 04_Basic_Queries.sql

---
+++ 04_Basic_Queries.sql
@@ -0,0 +1,10 @@
+-- Basic Queries
+SELECT * FROM Employees;
+
+SELECT * FROM Salary;
+
+SELECT employee_name,city FROM Employees;
+
+SELECT * FROM Employees WHERE city='Noida'; 
+
+SELECT * FROM Employees WHERE age>25;
